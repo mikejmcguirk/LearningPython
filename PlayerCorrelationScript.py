08bbe4f340522f48a9f78dac78cdfc56d628d5ab
mikejmcguirk <62523234+mikejmcguirk@users.noreply.github.com>
2020-05-04 00:33:33 -0400

    Add files via upload

--- PlayerCorrelationScript.py
+++ PlayerCorrelationScript.py
@@ -1,14 +1,19 @@
 #Intakes a set of player box scores from Basketball-Reference.com,
 #then checks for a correlation between player scoring and team victory
 
+def roundup(x):
+    return int(math.ceil(x / 10.0)) * 10
+
 import numpy as np
 from scipy.stats import pearsonr
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-teamResults = pd.read_csv('PlayerResults.csv')
+import math
 
+#Import, clean, and format data
+
+teamResults = pd.read_csv('PlayerResults.csv')
 results = np.array(teamResults['Unnamed: 7'])
 cleanResults = np.zeros_like(results, dtype = int)
 
@@ -30,24 +35,28 @@ for i in range(len(points)):
         points[i] = -1
 
 cleanPoints = points.astype(int)
-
 presentMask = cleanPoints >= 0
 
 correlators = pd.DataFrame({'PlayerPoints': cleanPoints[presentMask], \
     'TeamResults': cleanResults[presentMask]})
-    
-coefficient, _ = \
-    pearsonr(correlators['PlayerPoints'], correlators['TeamResults'])
-    
-#print(coefficient)
+
+#Plot data
 
 plt.scatter(correlators['PlayerPoints'], correlators['TeamResults'], \
     marker = 'o')
 plt.xlabel(correlators.columns[0])
 plt.ylabel(correlators.columns[1])
+plt.xlim([0, roundup(max(correlators['PlayerPoints']) + 5)])
+plt.ylim([0, roundup(max(correlators['TeamResults']) + 5)])
 
 z = np.polyfit(correlators['PlayerPoints'], correlators['TeamResults'], 1)
 p = np.poly1d(z)
 plt.plot(correlators['PlayerPoints'],p(correlators['PlayerPoints']),"r--")
+
+coefficient, _ = \
+    pearsonr(correlators['PlayerPoints'], correlators['TeamResults'])
+textstr = 'Correlation: ' + str(round(coefficient, 5))
+plt.text(5, max(correlators['TeamResults']) + 5, \
+    textstr, fontsize=14, verticalalignment='bottom')
 
 plt.show()
